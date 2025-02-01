c6f4752095f79be2698ca62c3137b702a8e8ebc1
Bruno Correa <correaricardobruno@gmail.com>
2025-02-01 12:04:53 -0300

    create revenues CRUD in memory - closes #2

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import salesRoutes from './routes/sales-routes.js';
+import revenuesRoutes from './routes/revenues-routes.js';
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', salesRoutes);
+app.use('/api', revenuesRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
---
+++ backend/controllers/revenues-controller.js
@@ -0,0 +1,58 @@
+let revenues = [];
+
+const getAllRevenues = (req, res) => {
+    res.json(revenues);
+}
+
+const createRevenue = (req, res) => {
+    const revenue = { ...req.body, id: revenues.length + 1 };
+
+    revenues.push(revenue);
+    res.status(201).json(revenue);
+}
+
+const getRevenueById = (req, res) => {
+    const { id } = req.params;
+    const revenue = revenues.find((revenue) => revenue.id === Number(id));
+
+    if (!revenue) {
+        return res.status(404).json({ message: 'Revenue not found' });
+    }
+
+    res.json(revenue);
+}
+
+const updateRevenue = (req, res) => {
+    const { id } = req.params;
+    const revenue = revenues.find((revenue) => revenue.id === Number(id));
+
+    if (!revenue) {
+        return res.status(404).json({ message: 'Revenue not found' });
+    }
+
+    const updatedRevenue = { ...revenue, ...req.body };
+    revenues = revenues.map((revenue) => (revenue.id === Number(id) ? updatedRevenue : revenue));
+
+    res.json(updatedRevenue);
+}
+
+const deleteRevenue = (req, res) => {
+    const { id } = req.params;
+    const revenue = revenues.find((revenue) => revenue.id === Number(id));
+
+    if (!revenue) {
+        return res.status(404).json({ message: 'Revenue not found' });
+    }
+
+    revenues = revenues.filter((revenue) => revenue.id !== Number(id));
+
+    res.json(revenue);
+}
+
+export {
+    getAllRevenues,
+    createRevenue,
+    getRevenueById,
+    updateRevenue,
+    deleteRevenue
+};
