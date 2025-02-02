ceb948d496e8ff6592e68b1ad316432023ea42d3
Bruno Correa <correaricardobruno@gmail.com>
2025-02-02 14:25:37 -0300

    fix deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,5 +7,5 @@ dotenv.config()
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: process.env.VITE_BASE_PATH_FRONTEND,
+    base: process.env.VITE_BASE_PATH_FRONTEND || "/sales-manager/",
 })
