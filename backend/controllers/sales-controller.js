22949cd0e147d679e79aff411c3e4f9e4bc156cf
Bruno Corrêa <correaricardobruno@gmail.com>
2025-05-05 22:13:58 -0300

    refactor: update sales data structure to use 'price' and 'location' fields

--- backend/controllers/sales-controller.js
+++ backend/controllers/sales-controller.js
@@ -1,124 +1,121 @@
 let sales = [
-    {
-        "id": 1,
-        "value": 10.65,
-        "quantity": 3,
-        "date": "16/06/2024",
-        "place": "Av. Paulista",
-        "paymentMethods": {
-            "creditCard": 0,
-            "debitCard": 0,
-            "cash": 0,
-            "pix": 3
-        }
+  {
+    id: 1,
+    price: 10.65,
+    quantity: 3,
+    date: "16/06/2024",
+    location: "Av. Paulista",
+    paymentMethods: {
+      creditCard: 0,
+      debitCard: 0,
+      cash: 0,
+      pix: 3,
     },
-    {
-        "id": 2,
-        "value": 139.05,
-        "quantity": 35,
-        "date": "28/05/2024",
-        "place": "Centro de Limeira",
-        "paymentMethods": {
-            "creditCard": 0,
-            "debitCard": 0,
-            "cash": 2,
-            "pix": 33
-        }
+  },
+  {
+    id: 2,
+    price: 139.05,
+    quantity: 35,
+    date: "28/05/2024",
+    location: "Centro de Limeira",
+    paymentMethods: {
+      creditCard: 0,
+      debitCard: 0,
+      cash: 2,
+      pix: 33,
     },
-    {
-        "id": 3,
-        "value": 120.24,
-        "quantity": 37,
-        "date": "06/05/2024",
-        "place": "Av. Paulista",
-        "paymentMethods": {
-            "creditCard": 0,
-            "debitCard": 0,
-            "cash": 2,
-            "pix": 35
-        }
+  },
+  {
+    id: 3,
+    price: 120.24,
+    quantity: 37,
+    date: "06/05/2024",
+    location: "Av. Paulista",
+    paymentMethods: {
+      creditCard: 0,
+      debitCard: 0,
+      cash: 2,
+      pix: 35,
     },
-    {
-        "id": 4,
-        "value": 66.27,
-        "quantity": 21,
-        "date": "19/02/2024",
-        "place": "Av. Paulista",
-        "paymentMethods": {
-            "creditCard": 0,
-            "debitCard": 0,
-            "cash": 14,
-            "pix": 7
-        }
+  },
+  {
+    id: 4,
+    price: 66.27,
+    quantity: 21,
+    date: "19/02/2024",
+    location: "Av. Paulista",
+    paymentMethods: {
+      creditCard: 0,
+      debitCard: 0,
+      cash: 14,
+      pix: 7,
     },
-    {
-        "id": 5,
-        "value": 82.64,
-        "quantity": 26,
-        "date": "25/12/2024",
-        "place": "Centro de Limeira",
-        "paymentMethods": {
-            "creditCard": 0,
-            "debitCard": 0,
-            "cash": 8,
-            "pix": 18
-        }
-    }
+  },
+  {
+    id: 5,
+    price: 82.64,
+    quantity: 26,
+    date: "25/12/2024",
+    location: "Centro de Limeira",
+    paymentMethods: {
+      creditCard: 0,
+      debitCard: 0,
+      cash: 8,
+      pix: 18,
+    },
+  },
 ];
 
-const getAllSales = (req, res) => {
-    res.json(sales);
-}
+import client from "../db.js";
+
+const getAllSales = async (req, res) => {
+  const sales = await client.query("SELECT * FROM sales_manager.sales");
+  res.json(sales.rows);
+};
 
 const createSale = (req, res) => {
-    const sale = { ...req.body, id: sales.length + 1 };
+  const sale = { ...req.body, id: sales.length + 1 };
 
-    sales.push(sale);
-    res.status(201).json(sale);
-}
+  sales.push(sale);
+  res.status(201).json(sale);
+};
 
 const getSaleById = (req, res) => {
-    const { id } = req.params;
-    const sale = sales.find((sale) => sale.id === Number(id));
+  const { id } = req.params;
+  const sale = sales.find((sale) => sale.id === Number(id));
 
-    if (!sale) {
-        return res.status(404).json({ message: 'Sale not found' });
-    }
+  if (!sale) {
+    return res.status(404).json({ message: "Sale not found" });
+  }
 
-    res.json(sale);
-}
+  res.json(sale);
+};
 
 const updateSale = (req, res) => {
-    const { id } = req.params;
-    const sale = sales.find((sale) => sale.id === Number(id));
+  const { id } = req.params;
+  const sale = sales.find((sale) => sale.id === Number(id));
 
-    if (!sale) {
-        return res.status(404).json({ message: 'Sale not found' });
-    }
+  if (!sale) {
+    return res.status(404).json({ message: "Sale not found" });
+  }
 
-    const updatedSale = { ...sale, ...req.body };
-    sales = sales.map((sale) => (sale.id === Number(id) ? updatedSale : sale));
+  const updatedSale = { ...sale, ...req.body };
+  sales = sales.map((sale) => (sale.id === Number(id) ? updatedSale : sale));
 
-    res.json(updatedSale);
-}
+  res.json(updatedSale);
+};
 
 const deleteSale = (req, res) => {
-    const { id } = req.params;
-    const sale = sales.find((sale) => sale.id === Number(id));
+  const { id } = req.params;
+  const sale = sales.find((sale) => sale.id === Number(id));
 
-    if (!sale) {
-        return res.status(404).json({ message: 'Sale not found' });
-    }
+  if (!sale) {
+    return res.status(404).json({ message: "Sale not found" });
+  }
 
-    sales = sales.filter((sale) => sale.id !== Number(id));
+  sales = sales.filter((sale) => sale.id !== Number(id));
 
-    res.json(sale);
-}
-
-export {
-    getAllSales,
-    createSale,
-    getSaleById,
-    updateSale,
-    deleteSale
+  res.json(sale);
 };
+
+export { getAllSales, createSale, getSaleById, updateSale, deleteSale };
